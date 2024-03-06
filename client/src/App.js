dc5b41fcd64d9c2ee82d0d323e4e6afd5a737eed
Daniel Lee <dale4990@colorado.edu>
2024-03-06 01:49:05 -0700

    comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './styles/App.css';
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, useNavigate, Routes} from 'react-router-dom';
+//import { BrowserRouter as Router, Route, useNavigate, Routes} from 'react-router-dom';
 import Axios from "axios";
 import Display from "./Display.js"
 // import { StyledInput, StyledButton } from './StyledComponents';
