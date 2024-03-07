f322fe0c0450cf775ca8b2a7705d61adf2422495
dale4990 <dale4990@colorado.edu>
2024-03-06 21:59:35 -0700

    fixed routing error

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
 import './styles/App.css';
-import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, useNavigate, Routes} from 'react-router-dom';
-import Axios from "axios";
-import Display from "./Display.js"
-// import { StyledInput, StyledButton } from './StyledComponents';
+import Routes from './Router/Routes.js'
 
 function App() {
   return(
-    <Routes/>
+    <Routes />
   );
 }
 
