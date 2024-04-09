698eb953e478e62fb6bc43b062335bd8012e7b3a
dorjeezzz <dj.namjyal@gmail.com>
2024-04-08 22:21:20 -0600

    Start to parse champions.json in champions page

--- client/src/Pages/Champions.js
+++ client/src/Pages/Champions.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import '../styles/champions.css';
+import championData from './assets/data/en_US/champion.json'
 
-function Champions() {
+const Champions = () => {
+  // Assuming championData has a "data" property that contains all champions
+  const champions = championData.data;
+
   return (
-    <div className="Champions">
-      <h1>Champions Page</h1>
-      <p>This is the Champions page, where details about champions will be displayed.</p>
+    <div>
+      {Object.keys(champions).map((championKey) => {
+        const champion = champions[championKey];
+        return (
+          <div key={champion.id} className="champion">
+            <h2>{champion.name}</h2>
+            <p>{champion.title}</p>
+            <img src={require(`./assets/img/${champion.image.full}`).default} alt={champion.name} />
+
+            {/* <img src={`/assets/images/${champion.image.full}`} alt={champion.name} /> */}
+            <p>{champion.blurb}</p>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default Champions;
