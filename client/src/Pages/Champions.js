9b8cb51af1720448dd8509dbc733b43e3c132e2e
dale4990 <dale4990@colorado.edu>
2024-04-09 23:32:50 -0600

    fixed bug in champions page

--- client/src/Pages/Champions.js
+++ client/src/Pages/Champions.js
@@ -3,7 +3,6 @@ import '../styles/champions.css';
 import championData from '../assets/data/en_US/champion.json'
 
 const Champions = () => {
-  // Assuming championData has a "data" property that contains all champions
   const champions = championData.data;
 
   return (
@@ -15,7 +14,7 @@ const Champions = () => {
             <h2>{champion.name}</h2>
             <p>{champion.title}</p>
             {/* <img src={require(`./assets/img/${champion.image.full}`).default} alt={champion.name} /> */}
-            <img src={`./public/splash/${champion.image.full}`} alt={champion.name} />
+            <img src={`/splash/${champion.id}_0.jpg`} alt={champion.name} />
             <p>{champion.blurb}</p>
           </div>
         );
