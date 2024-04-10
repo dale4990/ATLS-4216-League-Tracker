724652f57672e22c52a31ffa6608427724c58a2d
Spasznee <codyyan285@gmail.com>
2024-04-09 18:29:05 -0600

    moved dictionary to higher class

--- client/src/LOLGGRouter.js
+++ client/src/LOLGGRouter.js
@@ -11,11 +11,29 @@ const summonerDict = require('./assets/data/en_US/summoner.json');
 const summoners = summonerDict.data; // dictionary of summoner spells
 const runesDict = require('./assets/data/en_US/runesReforged.json'); // array of runes
 
+const parseChampionData = () => {
+    const championImageMap = {};
+  
+    for (const championKey in champions) {
+      if (Object.hasOwnProperty.call(champions, championKey)) {
+        const champion = champions[championKey];
+        const championId = champion.key;
+        const championImage = champion.image.full;
+        championImageMap[championId] = championImage;
+      }
+    }
+  
+    return championImageMap;
+};
+
+const championImageMap = parseChampionData();
+
 // Pack them together
 export const data = {
     champions: champions,
     summoners: summoners,
-    runes: runesDict
+    runes: runesDict,
+    championImageMap: championImageMap,
 };
 
 function LOLGGRouter() {
