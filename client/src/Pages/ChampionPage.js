e3bef5b6bb76ee675961cb17461603a4d04bb07f
dorjeezzz <dj.namjyal@gmail.com>
2024-04-15 17:55:28 -0600

    Added correct routing for each individual champion's page splash art and lore

--- client/src/Pages/ChampionPage.js
+++ client/src/Pages/ChampionPage.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const ChampionPage = () => {
-    const { championName } = useParams(); 
-    console.log(championName);
-  
+    const { championName } = useParams();
+    const [championData, setChampionData] = useState(null);
+    const splaaash = `/splash/${championName}_0.jpg`;
+    useEffect(() => {
+        const fetchChampionData = async () => {
+            try {
+                const data = await import(`../assets/data/en_US/champion/${championName}.json`);
+                setChampionData(data.data[championName]);
+            } catch (error) {
+                console.error('Error msg: ', error);
+                setChampionData(null);
+            }
+        };
+        fetchChampionData();
+    }, [championName]);
+
     return (
-      <div>
-        <h1>Champion Page for {championName}</h1>
-      </div>
+        <div>
+            <h1>Champion Page for {championData?.name || championName}</h1>
+            <img src={splaaash} className="champion-img" alt={`${championName} Splash Art`} />
+            <p>{championData ? championData.lore : 'Loading lore...'}</p>
+        </div>
     );
 };
 
