e08a9e282dfaf1fdce22381629f9caa0fb3e1bda
Daniel Lee <dale4990@colorado.edu>
2024-04-15 21:37:07 -0600

    styling changes

--- client/src/Pages/Modes.js
+++ client/src/Pages/Modes.js
@@ -6,22 +6,26 @@ import aram from '../images/aram.jpeg';
 
 function Modes() {
   return (
-    <div className="modes-container">
-      <h1>Modes</h1>
-      <div className="mode-box">
-        <img src={sr} alt="Summoners Rift" className="mode-image" />
-        <div className="mode-header">Casual</div>
-        <p>Classic League SR</p>
+    <div className="modes">
+      <div>
+        <h1>Modes</h1>
       </div>
-      <div className="mode-box">
-        <img src={tft} alt="Ranked Solo/Duo mode" className="mode-image" />
-        <div className="mode-header">TFT</div>
-        <p>Kchen's addicted to both porn and TFT</p>
-      </div>
-      <div className="mode-box">
-        <img src={aram} alt="ARAM mode" className="mode-image" />
-        <div className="mode-header">ARAM</div>
-        <p>ARAM, Americans Randomly All Mid</p>
+      <div className='modes-container'>
+        <div className="mode-box">
+          <img src={sr} alt="Summoners Rift" className="mode-image" />
+          <div className="mode-header">CLASSIC</div>
+          <p>Summoner's Rift</p>
+        </div>
+        <div className="mode-box">
+          <img src={aram} alt="ARAM mode" className="mode-image" />
+          <div className="mode-header">ARAM</div>
+          <p>All Random All Mid</p>
+        </div>
+        <div className="mode-box">
+          <img src={tft} alt="Ranked Solo/Duo mode" className="mode-image" />
+          <div className="mode-header">TFT</div>
+          <p>TeamFight Tactics</p>
+        </div>
       </div>
     </div>
   );
