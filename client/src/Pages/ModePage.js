f11bdd84bda457ea3b61dc86d5457a6aba2f54d4
Riley Mei <rileymmei@gmail.com>
2024-04-24 17:39:26 -0600

    template for diff mode pages

--- client/src/Pages/ModePage.js
+++ client/src/Pages/ModePage.js
@@ -3,12 +3,29 @@ import { useParams } from 'react-router-dom';
 
 const ModePage = () => {
     const { modeName } = useParams(); 
-  
-    return (
-      <div>
-        <h1>{modeName}</h1>
-      </div>
-    );
+
+    if (modeName === "Summoner's Rift") {
+      return (
+        <div>
+          <h1>{modeName}</h1>
+        </div>
+      );
+    }
+    else if (modeName === "ARAM") {
+      return (
+        <div>
+          <h1>{modeName}</h1>
+        </div>
+      );
+    }
+    else if (modeName === "TFT") {
+      return (
+        <div>
+          <h1>{modeName}</h1>
+        </div>
+      );
+    }
+    
 };
 
 export default ModePage;
