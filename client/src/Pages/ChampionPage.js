3e3adfa4f02b1334952b0813edaf86e0ea1c11fb
dale4990 <dale4990@colorado.edu>
2024-04-24 19:06:48 -0600

    champion page styling

--- client/src/Pages/ChampionPage.js
+++ client/src/Pages/ChampionPage.js
@@ -5,13 +5,13 @@ import '../styles/ChampPage.css';
 const ChampionPage = () => {
     const { championName } = useParams();
     const [championData, setChampionData] = useState(null);
-    const splaaash = `/splash/${championName}_0.jpg`;
+    const splaaash = `/splash/${championName === "Wukong" ? "MonkeyKing" : championName}_0.jpg`;
 
     useEffect(() => {
         const fetchChampionData = async () => {
             try {
-                const data = await import(`../assets/data/en_US/champion/${championName}.json`);
-                setChampionData(data.data[championName]);
+                const data = await import(`../assets/data/en_US/champion/${championName === "Wukong" ? "MonkeyKing" : championName}.json`);
+                setChampionData(data.data[championName === "Wukong" ? "MonkeyKing" : championName]);
             } catch (error) {
                 console.error('Error msg: ', error);
                 setChampionData(null);
@@ -26,15 +26,15 @@ const ChampionPage = () => {
 
     return (
       <div className='champion-page' style={backgroundStyle}>
-        {/* <div className="overlay"></div> */}
-        <div className="champion-content">
-            <h1>{championName}</h1>
-            <div className="champion-details">
-                <div className="Modes">Summoners Rift</div>
-                <div className="Modes">ARAM</div>
-            </div>
-            <div className="champion-lore">
-                <p>{championData ? championData.lore : 'Lore unavailable, champion is too recent...'}</p>
+        <div className="champion-content-container">
+            <div className="champion-content">
+                <h1>{championName.toUpperCase()}</h1>
+                <div className='champion-title'>
+                    <h2>{championData ? championData.title.toUpperCase() : 'Title unavailable, champion is too recent...'}</h2>
+                </div>
+                <div className="champion-lore">
+                    <p>{championData ? championData.lore : 'Lore unavailable, champion is too recent...'}</p>
+                </div>
             </div>
         </div>
       </div>
